ef38f19ff77514cb44f8047bff182c2f6cf84494
hyeri-woo <woohyeri0525@gmail.com>
2023-08-30 00:03:28 +0900

    Feat: Issue 타입으로 반환

--- src/api/issue.ts
+++ src/api/issue.ts
@@ -1,8 +1,19 @@
+import { Issue } from '../types';
 import instance from './index';
 
-const fetchGetIssue = async (): Promise<any> => {
+const fetchGetIssue = async (): Promise<Issue[]> => {
   const response = await instance.get('/repos/facebook/react/issues');
-  return response.data;
+  return response.data.map((issue: any) => {
+    return {
+      number: issue.number,
+      title: issue.title,
+      author: issue.user.login,
+      date: issue.created_at,
+      comments: issue.comments,
+      image: issue.user.avatar_url,
+      body: issue.body,
+    };
+  });
 };
 
 export { fetchGetIssue };
