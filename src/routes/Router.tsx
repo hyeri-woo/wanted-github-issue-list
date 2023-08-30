8c4fdf849ec7e29d0d60ec4d14cf11de9cce6240
hyeri-woo <woohyeri0525@gmail.com>
2023-08-30 19:40:58 +0900

    Feat: 라우터 에러 페이지 작동

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,9 +1,15 @@
 import Detail from '../pages/Detail';
+import Home from '../pages/Home';
 import Issue from '../pages/Issue';
 import NotFound from '../pages/NotFound';
 import { createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <NotFound />,
+  },
   {
     path: '/issue',
     element: <Issue />,
