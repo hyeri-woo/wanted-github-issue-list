18ed7949ed92dacc2141b3fc59cca13e5234f39b
hyeri-woo <woohyeri0525@gmail.com>
2023-08-30 20:43:04 +0900

    Feat: 광고 카드 기능 구현

--- src/components/common/AdCard.tsx
+++ src/components/common/AdCard.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export default function AdCard() {
-  return <StyledCard>ad</StyledCard>;
+  return (
+    <StyledCard>
+      <Link to='https://www.wanted.co.kr/jobsfeed'>
+        <img
+          src='https://github.com/hyeri-woo/wanted-pre-onboarding-frontend/assets/107099724/d5be6d63-52a6-4260-8e19-660ea350f05c'
+          alt='광고 배너'
+        />
+      </Link>
+    </StyledCard>
+  );
 }
 
-const StyledCard = styled.article``;
+const StyledCard = styled.article`
+  background: var(--color-white20);
+  border-radius: 30px;
+  border: 1px solid var(--color-white70);
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.25);
+  height: 150px;
+  overflow: hidden;
+  position: relative;
+  img {
+    position: absolute;
+    width: 50%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-58%, -50%);
+    object-fit: cover;
+  }
+`;
