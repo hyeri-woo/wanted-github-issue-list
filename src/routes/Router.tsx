2f8b84609e2b81a5d214c368ea0c0ae26df15745
hyeri-woo <woohyeri0525@gmail.com>
2023-08-30 20:44:10 +0900

    Fix: 라우터 변경 및 Home 페이지 삭제

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,5 +1,4 @@
 import Detail from '../pages/Detail';
-import Home from '../pages/Home';
 import Issue from '../pages/Issue';
 import NotFound from '../pages/NotFound';
 import { createBrowserRouter } from 'react-router-dom';
@@ -7,17 +6,16 @@ import { createBrowserRouter } from 'react-router-dom';
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
-    errorElement: <NotFound />,
-  },
-  {
-    path: '/issue',
     element: <Issue />,
     errorElement: <NotFound />,
   },
   {
-    path: '/issue/:number',
+    path: '/:number',
     element: <Detail />,
+  },
+  {
+    path: '/error',
+    element: <NotFound />,
   },
 ]);
 
