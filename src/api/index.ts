2d2ed1304047878d2bc3365332c605ff95e6f651
hyeri-woo <woohyeri0525@gmail.com>
2023-08-30 23:11:38 +0900

    Fix: token 삭제시 build 확인

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,9 +2,6 @@ import axios, { AxiosInstance } from 'axios';
 
 const instance: AxiosInstance = axios.create({
   baseURL: 'https://api.github.com',
-  headers: {
-    Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
-  },
 });
 
 export default instance;
