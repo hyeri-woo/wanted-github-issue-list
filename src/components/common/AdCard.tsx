76a525c01200e81f8d53c6f61ff94cce7c79c500
hyeri-woo <woohyeri0525@gmail.com>
2023-08-30 16:04:00 +0900

    Feat: 광고 배너 기본 완성

---
+++ src/components/common/AdCard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function AdCard() {
+  return <StyledCard>ad</StyledCard>;
+}
+
+const StyledCard = styled.article``;
