ae0c32ecea9f94f10b94674d712d4d9e80460c25
hyeri-woo <woohyeri0525@gmail.com>
2023-09-01 23:40:32 +0900

    Fix: Github Action build 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,19 +1,22 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
 const instance: AxiosInstance = axios.create({
   baseURL: 'https://api.github.com',
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+  },
 });
 
-instance.interceptors.request.use(
-  (config: AxiosRequestConfig): AxiosRequestConfig => {
-    config.headers = {
-      Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
-    };
-    return config;
-  },
-  (error: AxiosError): Promise<AxiosError> => {
-    return Promise.reject(error);
-  },
-);
+// instance.interceptors.request.use(
+//   (config: AxiosRequestConfig): AxiosRequestConfig => {
+//     config.headers = {
+//       Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+//     };
+//     return config;
+//   },
+//   (error: AxiosError): Promise<AxiosError> => {
+//     return Promise.reject(error);
+//   },
+// );
 
 export default instance;
