41e605dee11277497f1a1f767a3299eea6124cec
hyeri-woo <woohyeri0525@gmail.com>
2023-09-01 23:33:36 +0900

    Fix: add headers authorization

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,19 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from 'axios';
 
 const instance: AxiosInstance = axios.create({
   baseURL: 'https://api.github.com',
 });
+
+instance.interceptors.request.use(
+  (config: AxiosRequestConfig): AxiosRequestConfig => {
+    config.headers = {
+      Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    };
+    return config;
+  },
+  (error: AxiosError): Promise<AxiosError> => {
+    return Promise.reject(error);
+  },
+);
 
 export default instance;
